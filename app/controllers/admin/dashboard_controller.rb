87ba3c2e947ae713dc0f74250e65dbae1d77173a
zero1108 <523971430@qq.com>
2020-06-01 17:07:43 +0800

    fix

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,8 +5,8 @@ class Admin::DashboardController < AdminController
   def index
   	current_time = Time.now
   	@result = Statement.where(created_at: current_time.at_beginning_of_month..current_time).group(:type).sum(:price)
-  	@income = @result["IncomeStatement"]
-  	@outcome = @result["OutcomeStatement"]
+  	@income = @result.fetch("IncomeStatement", 0)
+  	@outcome = @result.fetch("OutcomeStatement", 0)
   end
 
 end
