bd6690728970cb2d02c7647e8b7071552469ad13
zero1108 <523971430@qq.com>
2020-06-01 13:11:42 +0800

    adjust home css

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,6 +4,7 @@ class Contact < ApplicationRecord
     
   acts_as_enum :state, %W(new dealed), %W(待处理 已处理)
   acts_as_enum :grade, %W(none 0 5 4 3 2 1), %W(未填写 零基础 N5 N4 N3 N2 N1)
+  acts_as_enum :target, %W(none travel study work grade), %W(未填写 出国旅游 出国留学 日企工作 学业考级)
 
   state_machine :state, :initial => :new do
     event :deal do
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,6 @@
 class Course < ApplicationRecord
 
   validates_presence_of :name, :introduction
-  validates :price, presence: true, numericality: { greater_than: 0 }
   validates :position, presence: true, numericality: { only_integer: true }
 
 end
